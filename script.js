4810f48c18f5a87cb92aaebe2af66a0748f70a3a
Wina Adeyeye <femiadeyeye@gmail.com>
2024-10-14 12:20:49 +0100

    cleared used code

--- script.js
+++ script.js
@@ -45,29 +45,6 @@ document.querySelector('.check').addEventListener('click', function () {
       displayScore(0);
     }
   }
-
-  //   //when is too high
-  // else if (guess > secretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector('.message').textContent = '📈 Too High!';
-  //     score--;
-  //     document.querySelector('.score').textContent = score;
-  //   } else {
-  //     document.querySelector('.message').textContent = '💥 You lost the game';
-  //     document.querySelector('.score').textContent = 0;
-  //   }
-
-  //   // when is too low
-  // } else if (guess < secretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector('.message').textContent = '📉 Too Low!';
-  //     score--;
-  //     document.querySelector('.score').textContent = score;
-  //   } else {
-  //     document.querySelector('.message').textContent = '💥 You lost the game';
-  //     document.querySelector('.score').textContent = 0;
-  //   }
-  // }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
@@ -82,7 +59,3 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
-////////////////////////////////////////
-// Coding Challenge #1
-
-/** */
